3aaebafba2b9d0c60235dfff09cde7b82a0c9600
Kevin Anderson <kmichael419@hotmail.com>
2020-07-27 13:26:21 -0500

    speed control increase and decrease buttons are working properly

--- src/components/SpeedControl/SpeedControl.js
+++ src/components/SpeedControl/SpeedControl.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 // THIS COMPONENT IS OUR INTERFACE FOR SPEED
 // YOU SHOULD DISPLAY THE CURRENT SPEED
 // BUTTONS SHOULD INCREASE OR DECREASE SPEED, RESPECTIVELY
 
 class SpeedControl extends Component {
+  handleClickIncrease = (event) => {
+    this.props.dispatch({ type: 'INCREASE_SPEED' });
+  };
+
+  handleClickDecrease = (event) => {
+    this.props.dispatch({ type: 'DECREASE_SPEED' });
+  };
+
   render() {
     return (
       <div>
         <h2>Speed Control</h2>
 
-        <button>Increase Speed</button>
+        <button onClick={this.handleClickIncrease}>Increase Speed</button>
         <p>SPEED: GOES HERE</p>
-        <button>Decrease Speed</button>
+        <button onClick={this.handleClickDecrease}>Decrease Speed</button>
       </div>
-    )
+    );
   }
 }
 
-export default SpeedControl;
+export default connect()(SpeedControl);
--- src/index.js
+++ src/index.js
@@ -2,17 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
 // put your reducers here!
 
+const speedReducer = (state = 0, action) => {
+  if (action.type === 'INCREASE_SPEED') {
+    return state + 1;
+  } else if (action.type === 'DECREASE_SPEED') {
+    return state - 1;
+  }
+
+  return state;
+};
+
+const passengerReducer = (state = '', action) => {
+  if (action.type === 'ADD_PASSENGER') {
+    return action.payload;
+  }
+
+  return state;
+};
 
 // be sure to combine your reducers!
 const storeInstance = createStore(
+  combineReducers({
+    speedReducer,
+    passengerReducer,
+  }),
   // reducers,
   applyMiddleware(logger)
-)
+);
 
-ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
